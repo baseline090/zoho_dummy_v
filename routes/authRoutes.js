72082e47928b563345131132f03e00704bb0168a
baseline090 <100409036+baseline090@users.noreply.github.com>
2024-11-25 12:06:12 +0530

    Update authRoutes.js

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,19 +1,4 @@
 
-// const express = require('express');
-// const router = express.Router();
-// const zCrmController = require('../controllers/z_crm_controller'); // Require the entire controller file
-
-// // Route to fetch leads from Zoho CRM
-// router.get('/fetch/leads', zCrmController.fetchLeadsData);
-
-// // Route to fetch and store leads in the database
-// router.get('/fetch/storedb', zCrmController.fetchLeadsDataAndStoreInDB);
-
-// // Route to fetch emails and company names from the database
-// router.get('/fetch/emails-companies', zCrmController.fetchEmailsAndCompanies);  // Assuming you added fetchEmailsAndCompanies in the z_crm_controller.js
-
-// module.exports = router;
-
 const express = require('express');
 const router = express.Router();
 const zCrmController = require('../controllers/z_crm_controller'); // Require the entire controller file
@@ -26,5 +11,8 @@ router.get('/fetch/storedb', zCrmController.fetchAndStoreLeadsData);
 
 // Route to fetch emails and company names from the database
 router.get('/fetch/emails-companies', zCrmController.fetchEmailsAndCompanies);
+
+// Route to fetch owner from the database
+router.get('/fetch/owners', zCrmController.fetchOwners);
 
 module.exports = router;
